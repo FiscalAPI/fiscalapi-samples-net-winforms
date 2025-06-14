197510e4b64edd2659c0def6e2cbf9805e39b0a1
JesusMendoza <mendoza.git@gmail.com>
2025-06-14 11:24:11 -0600

    using removed

--- FiscalApi.Samples.NetFramework/Form1.cs
+++ FiscalApi.Samples.NetFramework/Form1.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Windows.Forms;
-using Fiscalapi.Abstractions;
 using Fiscalapi.Common;
 using Fiscalapi.Models;
 using Fiscalapi.Services;
