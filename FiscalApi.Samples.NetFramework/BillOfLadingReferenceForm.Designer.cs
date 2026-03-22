b10b6ea4fc6b15c2a2cdb93fc35afe6ab358fdcc
Jose Antonio Medina <medina.octubre95@live.com.mx>
2026-03-22 12:24:59 -0600

    Fixed typos

--- FiscalApi.Samples.NetFramework/BillOfLadingReferenceForm.Designer.cs
+++ FiscalApi.Samples.NetFramework/BillOfLadingReferenceForm.Designer.cs
@@ -169,7 +169,7 @@
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(189, 60);
             this.button7.TabIndex = 7;
-            this.button7.Text = "Factura ingresdo transporte ferrovirario internacional aduanero";
+            this.button7.Text = "Factura ingreso transporte ferroviario internacional aduanero";
             this.button7.UseVisualStyleBackColor = true;
             this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
